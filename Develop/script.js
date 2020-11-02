a414497cb9c72cba445990cdcabb869512db0fc1
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-01 21:40:42 -0500

    updated javascript to prompt password length and check it is between 8-128

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function askQuestions(){
+  var passwordLength = prompt("How long should we make your password?");
+  
+  if(passwordLength < 8){
+    alert("Must be at least 8 characters!");
+  } else if (passwordLength > 128) {
+    alert("Must be 128 characters or less!");
+  }
+
+  var includeUpper = confirm("Include upper case letters?");
+  var includeLower = confirm("Include lower case letters?"):
+  var includeNumbers = confirm("Include numbers?");
+  var includeSpecial = confirm("Include special characters?");
+}
+
+function generatePassword(){
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
