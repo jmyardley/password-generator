5093c717fa3e7ad5d1e53c9ee97a143a403c281c
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-02 18:12:19 -0500

    return user prompt answers for generate pw function

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ var lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var specials = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[']
 
+//Prompts to user
 function askQuestions(){
   var passwordLength = prompt("How long should we make your password?");
   
   if(passwordLength < 8){
     alert("Must be at least 8 characters!");
+    return;
   } else if (passwordLength > 128) {
     alert("Must be 128 characters or less!");
+    return;
   }
 
   var includeUppers = confirm("Include upper case letters?");
@@ -21,7 +24,8 @@ function askQuestions(){
   var includeNumbers = confirm("Include numbers?");
   var includeSpecials = confirm("Include special characters?");
   if (includeUppers === false && includeLowers === false && includeNumbers === false && includeSpecials === false) {
-    alert("Must include at least one uppercase letter, lowercase letter, special character, or number.")
+    alert("Must include at least one uppercase letter, lowercase letter, special character, or number.");
+    return;
   }
 
   console.log(includeUppers);
@@ -29,10 +33,40 @@ function askQuestions(){
   console.log(includeNumbers);
   console.log(includeSpecials);
 
+  var answers = {
+    useUppers: includeUppers,
+    useLowers: includeLowers,
+    useNumbers: includeNumbers,
+    useSpecials: includeSpecials,
+    useLength: passwordLength
+  }
+
+  return answers;
 }
 
 
 function generatePassword(){
+
+  var userAnswers = askQuestions();
+  var useCharacters = [];
+
+  console.log(userAnswers);
+
+  if (userAnswers.useUppers){
+    console.log("Include upper case letters");
+  }
+  if (userAnswers.useLowers){
+    console.log("Include lower case letters");
+  }
+  if (userAnswers.useNumbers){
+    console.log("Include numbers");
+  }
+  if (userAnswers.useSpecials){
+    console.log("Include special chars");
+  }
+
+
+
 
 }
 
@@ -46,4 +80,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", askQuestions);
+generateBtn.addEventListener("click", writePassword);
