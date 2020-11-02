6cef5d38338a9aa2ffdbd69cbc832fafdb61242b
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-02 17:43:44 -0500

    added character arrays and console logged user answers

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Arrays of potential characters
+var uppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var specials = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[']
+
 function askQuestions(){
   var passwordLength = prompt("How long should we make your password?");
   
@@ -10,14 +16,21 @@ function askQuestions(){
     alert("Must be 128 characters or less!");
   }
 
-  var includeUpper = confirm("Include upper case letters?");
-  var includeLower = confirm("Include lower case letters?");
+  var includeUppers = confirm("Include upper case letters?");
+  var includeLowers = confirm("Include lower case letters?");
   var includeNumbers = confirm("Include numbers?");
-  var includeSpecial = confirm("Include special characters?");
-  if (includeUpper === false && includeLower === false && includeNumbers === false && includeSpecial === false) {
+  var includeSpecials = confirm("Include special characters?");
+  if (includeUppers === false && includeLowers === false && includeNumbers === false && includeSpecials === false) {
     alert("Must include at least one uppercase letter, lowercase letter, special character, or number.")
   }
+
+  console.log(includeUppers);
+  console.log(includeLowers);
+  console.log(includeNumbers);
+  console.log(includeSpecials);
+
 }
+
 
 function generatePassword(){
 
