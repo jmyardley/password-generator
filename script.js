f77fb61c5e76e90e0d590d4bd8256326a0a2b86b
James Y <35856061+jmyardley@users.noreply.github.com>
2020-11-02 22:04:11 -0500

    add more special chars, add array for checking that each requested character type is included

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 var uppers = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-var specials = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[']
+var specials = ['!','"','#','$','%','&',',',')','(','*','+','-','.','/',':',';','<','=','>','?','@','[',']','^','_','{','}','|','~'] 
 
 //Prompts to user
 function askQuestions(){
@@ -95,6 +95,8 @@ function generatePassword(){
   return realPassword;
 
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
